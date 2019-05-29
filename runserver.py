b749991dae9787778962bc63b4060f549f5430e7
Chen Ying <5565191487@qq.com>
2019-05-29 11:18:15 +0800

    test6

--- runserver.py
+++ runserver.py
@@ -1,4 +1,4 @@
 # -*- encoding=UTF-8 -*-
 from nowstagram import app
 if __name__ == '__main__':
-    app.run(host='192.168.181.128', debug = True)
+    app.run( host='0.0.0.0',port=80, debug = True)
